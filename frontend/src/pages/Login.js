7d767eeecfa5b3e09b59da8026e6b94b614e42cc
whyme <ritikshrestha94@gmail.com>
2026-01-18 21:33:05 +0545

    added content in the login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,27 @@
-import {Container} from '@mui/material';
+import {Container, FormControl, Input, InputLabel} from '@mui/material';
+import signinimg from '../assets/signin.png';
+import './login.css';
 
 export function LoginPage(){
     return (
         <>
-        <Container>
-            <h1>User Login Page</h1>
-        </Container>
+        <div className="container">
+            <div className="img-container">
+                <img src={signinimg} alt="login-img" className='login-img'/>
+            </div>
+            <div className="login-form-container">
+                <div className="login-form">
+                    <h1>WELCOME BACK </h1>
+                    <form action="#" method = "POST">
+                        <input type="text" name='username' placeholder='Username'/>
+                        <input type="password" name='username' placeholder='Password' />
+                        <small>Forget Password?</small>
+                        <button type='submit' className='login-btn'>LOGIN</button>
+                    </form>
+                    <p>Don't have an account? <strong>Sign Up!</strong></p>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
