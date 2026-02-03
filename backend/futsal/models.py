7a7f8cb2952ff3f1343a9893d6183fb0d3c07ead
whyme <ritikshrestha94@gmail.com>
2026-02-03 20:34:52 +0545

    adding booking model and slight changes in finding futsal page

--- backend/futsal/models.py
+++ backend/futsal/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 
 class Futsal(models.Model):
@@ -7,8 +8,20 @@ class Futsal(models.Model):
     no_of_grounds = models.IntegerField(default=1)
     description = RichTextField()
     slug = models.SlugField(default='', null= False)
+    
 
 
     def __str__(self):
         return f'{self.name} at {self.location}'
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length = 50, null = True, blank = True)
+    booking_day = models.CharField(null = True, blank = True)
+    booking_slot = models.CharField(null = True, blank = True)
+    futsal = models.ForeignKey(Futsal, on_delete=models.PROTECT, related_name='futsal')
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='user' )
+
+    def __str__(self):
+        return f'Nooked by {self.name} at {self.futsal}'
 
--- frontend/src/pages/FindFutsal.js
+++ frontend/src/pages/FindFutsal.js
@@ -1,8 +1,32 @@
 import {Container, TextField, Button, InputAdornment, IconButton} from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import './findfutsal.css';
+import futsalPic from '../assets/futsal.png';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
+import './futsal.css';
+import ground from '../assets/groundgreen.png';
+
+import { Link } from 'react-router-dom';
 
 export function FindFutsal(){
+
+    const [futsals, setFutsal] = useState([]);
+    useEffect(()=>{
+        const listFutsals = async ()=>{
+            try{
+                const response = await axios.get("http://127.0.0.1:8000/api/futsals/");
+                setFutsal(response.data);
+                console.log(response.data);
+            }
+            catch(e){
+                console.log("Error occured!" , e)
+            }
+        };
+        listFutsals();
+    }, [])
     return (
         <>
         <Container sx={{marginTop:'5em'}}>
@@ -63,6 +87,29 @@ export function FindFutsal(){
                     </div>
                 </div>
             </div>
+        </Container>
+        <Container sx={{marginTop:'2em'}}>
+            <div className="futsal-container">
+                <h2 className="recommnded-futsal-header">Recommended Futsals</h2>
+                <div className="futsal-list">
+                    {futsals.map(futsal => (
+                
+                    <div className="futsal-card" key={futsal.id}>
+                        <img src={futsalPic} alt="fustal-img" className='futsal-image' />
+                        <div className="futsal-info">
+                            <h2>{futsal.name}</h2>
+                            <h5> <LocationOnIcon sx={{color:'green'}}/> {futsal.location}</h5>
+                            <h5> <LocalPhoneOutlinedIcon sx={{color:'green'}}/> 01-4324322</h5>
+                            <h5> <img src={ground} alt="futsal-icon" width={'20px'} /> {futsal.no_of_grounds} grounds</h5>
+                        </div>
+                        <Link to={`/futsals/${futsal.slug}`} style={{textDecoration:'none', color:'black'}}>
+                        <button className='detail-btn'>View In Detail</button>
+                        </Link>
+                    </div>
+                    ))}
+                </div>
+            </div>
+            
         </Container>
         </>
     )
